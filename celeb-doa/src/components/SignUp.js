d6b21f3b0de5fb06f62972df1c2184f3a2bb6756
krunal Patel <pkrunal616@gmail.com>
2019-10-21 17:32:48 -0400

    sign-up

--- celeb-doa/src/components/SignUp.js
+++ celeb-doa/src/components/SignUp.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { registerUser } from '../actions';
+
+const SignUp = (props) =>{
+
+    const [register, setRegister] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+        props.registerUser(register)
+        setRegister({
+            username: '',
+            password: ''
+        })
+    }
+
+    const handleChange = e =>{
+        setRegister({...register, [e.target.name]: [e.target.value]})
+    }
 
 
-const SignUp = () =>{
     return(
         <div>
-            <h1>Sign UP</h1>
+
+            <form onSubmit={handleSubmit}>
+                <input
+                    className='register'
+                    type='text'
+                    placeholder='Username'
+                    value={register.username}
+                    onChange={handleChange}
+                />
+                <input
+                    className='register'
+                    type='password'
+                    placeholder='Password'
+                    value={register.password}
+                    onChange={handleChange}
+                />
+                <button type='submit'>Sign Up</button>
+            </form>
+            
         </div>
     )
 }
