2af15045818e11f2f9a76477b284cbbe142f1c0f
krunal Patel <pkrunal616@gmail.com>
2019-10-21 15:56:19 -0400

    Rouing and links

--- celeb-doa/src/components/Nav.js
+++ celeb-doa/src/components/Nav.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
+import styled from 'styled-components';
+
 
 const Nav = () =>{
 
     return(
         <div>
+
+            <div>
+                <h1>Celebrity?</h1>
+                <h3>Dead or Alive</h3>
+            </div>
             
             <div>
                 <NavLink to='/'>Quiz</NavLink>
