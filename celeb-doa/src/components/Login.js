e3732f49b16dd60f625b4622d1d76553ddb511c6
Greyflanel <richardlovelace811@gmail.com>
2019-10-21 15:51:49 -0500

    Login form is complete and waiting to be hooked up to store.

--- celeb-doa/src/components/Login.js
+++ celeb-doa/src/components/Login.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from "react";
+import { loginUser } from "../actions";
 
-const Login = () => {
-    return(
+const Login = props => {
+  const [login, setLogin] = useState({ username: "", password: "" });
 
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
-}
+  const handleChange = event => {
+    setLogin({ ...login, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    props.loginUser(login);
+    setLogin({ username: "", password: "" });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="loginForm"
+          type="text"
+          name="username"
+          placeholder="name"
+          value={login.username}
+          onChange={handleChange}
+        />
+        <input className="loginForm" 
+        type="password" 
+        name="password" 
+        value={login.password} 
+        onChange={handleChange} 
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
