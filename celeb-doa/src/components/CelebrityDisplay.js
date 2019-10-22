26f73536c636b1ea309abe089a20f112b666db96
alec blakeley <alecblkly@gmail.com>
2019-10-22 17:38:50 -0400

    Created celebrity delete functionality. Current CORS policy issue, will revisit

--- celeb-doa/src/components/CelebrityDisplay.js
+++ celeb-doa/src/components/CelebrityDisplay.js
@@ -1,7 +1,21 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const CelebrityDisplay = props => {
+
+  const deleteCeleb = () => {
+    axiosWithAuth()
+    .delete(`http://celeb-death-status.herokuapp.com/api/protected/celebs/${props.celeb.id}`)
+    .then(response => {
+      console.log("Delete was successful", response)
+      // props.history.push(`/celebrity-list`)
+    })
+    .catch(error => {
+      console.log(error.response)
+    })
+  }
+
   return (
     <div className="celebrity-info">
       <h1>{props.celeb.name}</h1>
@@ -14,6 +28,7 @@ const CelebrityDisplay = props => {
       >
         Edit Celebrity
       </button>
+      <button onClick={deleteCeleb}>Delete Celebrity</button>
     </div>
   );
 };
